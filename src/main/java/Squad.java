d07a7bd1b85e379b21e49f52d58d8a00934c1fc3
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-09-16 17:12:52 +0200

    add  more method

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -3,35 +3,21 @@ import java.util.List;
 import java.util.Objects;
 
 public class Squad{
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Squad squad = (Squad) o;
-        return sSize == squad.sSize &&
-                id == squad.id &&
-                Objects.equals(sName, squad.sName) &&
-                Objects.equals(sCause, squad.sCause);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(sName,sCause,sSize, id);
-    }
     private String sName;
-    private  String sCause;
+    private  String cause;
     private int id;
-    private  int sSize;
+    private  int size;
 private static ArrayList<Squad>instance=new ArrayList<Squad>();
 
 private List<Hero>hHeroes;
 
-    public Squad(String name, String cause,int size) {
-       sName=name;
-      sCause=cause;
-      sSize=size;
+    public Squad(String sname, String cause,int size) {
+       this.sName=sname;
+      this.cause=cause;
+      this.size=size;
       instance.add(this);
       hHeroes=new ArrayList<Hero>();
+        this.id=instance.size();
     }
 
     public String getsName() {
@@ -40,7 +26,7 @@ private List<Hero>hHeroes;
     }
 
     public int getsSize() {
-        return sSize;
+        return size;
     }
 
     public List<Hero> gethHeroes(){
@@ -49,7 +35,7 @@ private List<Hero>hHeroes;
 }
     public String getsCause() {
 
-        return sCause;
+        return cause;
     }
 public static List<Squad>all(){
 
@@ -60,7 +46,7 @@ public static List<Squad>all(){
         this.id = id;
     }
 
-    public static Squad find(int id) {
+    public static Squad findSquadById(int id) {
         return instance.get(id-1);
     }
     public int getId() {
@@ -68,6 +54,7 @@ public static List<Squad>all(){
         return id;
     }
     public void addHero(Hero hero) {
+
         hHeroes.add(hero);
     }
     public static boolean heroAlreadyExists(Hero newHero) {
