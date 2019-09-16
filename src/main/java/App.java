feedf13ec8a52d319d3bf8544af0063a4c4fe1b9
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-09-16 17:11:00 +0200

    add more path

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -14,52 +14,46 @@ public class App {
         get("/", (req, res) -> {
             Map<String, Object> model = new HashMap<String, Object>();
             List<Hero> hero = Hero.all();
-           List<Squad> squad =Squad.all();
+           List<Squad> squads =Squad.all();
            model.put("hero",hero);
-           model.put("squad",squad);
+           model.put("squads",squads);
             return new ModelAndView(model, "indexe.hbs");
         }, new HandlebarsTemplateEngine());
 
-
-        // getting all heroes  showing all hero details
-        get("/heroes", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            model.put("heroes", Hero.all());
-//            model.put("template", "templates/heroes.vtl");
-            return new ModelAndView(model, "hero.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        //getting heroes by their id which was returned by find method //
-
-        get("/heroes/:hId", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            Hero hero = Hero.find(Integer.parseInt(request.params(":hId")));
-            model.put("hero", hero);
-
-            return new ModelAndView(model, "hero.hbs");
-        }, new HandlebarsTemplateEngine());
 
         get("/heros/new",(request, response) -> {
             Map<String,Object> model = new HashMap<>();
             return new ModelAndView(model,"hero-form.hbs");
 
         },new HandlebarsTemplateEngine());
+
         //task: process new hero form
         post ("/heroes/new",(request, response) -> {
             Map<String,Object>model = new HashMap<>();
             String name =request.queryParams("name");
-            int age = Integer.parseInt(request.queryParams("age"));
-            String power= request.queryParams("power");
-            String weakness = request.queryParams("weakness");
-
+            int age=Integer.parseInt(request.queryParams("age"));
+            String power=request.queryParams("power");
+            String weakness=request.queryParams("weakness");
+                  System.out.print(name);
             Hero newIdentity =new Hero(name,age,power,weakness);
-            model.put("hero",newIdentity);
+            model.put("heroes",newIdentity);
             return new ModelAndView(model,"hero-success.hbs");
         }, new HandlebarsTemplateEngine());
 
+        //getting heroes by their id which was returned by find method //
+        
+        get("/heroes/:hid", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            int heroIdToFind=Integer.parseInt(request.params("hId"));
+            Hero findHero = Hero.findById(heroIdToFind);
+            model.put("hero", findHero);
+
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
         get("/squads/new", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-
             return new ModelAndView(model, "squad-form.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -67,44 +61,39 @@ public class App {
 
         post("/squads/list", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            String nameSquad = request.queryParams("nameSquad");
-            String causeSquad = request.queryParams("causeSquad");
-            int sizeSquad = Integer.parseInt(request.queryParams("sizeSquad"));
-            Squad newSquad= new Squad(nameSquad,causeSquad,sizeSquad);
-            model.put("squad",newSquad);
+            String sName = request.queryParams("sname");
+            String cause=request.queryParams("cause");
+            int size=Integer.parseInt(request.queryParams("size"));
+
+                Squad newIdentitySquad =new Squad(sName,cause,size);
+                model.put("squads",newIdentitySquad);
             return new ModelAndView(model, "squadSuccessful.hbs");
         }, new HandlebarsTemplateEngine());
 
-
-
         get("/squads/list", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            List<Squad> squad =  Squad.all();
-            model.put("squad", squad);
-//            model.put("template", "templates/squads.vtl");
             return new ModelAndView(model,"squads.hbs" );
         }, new HandlebarsTemplateEngine());
 
         //route to handle displaying found squad returned by the find method in class Hero
         get("/squads/:id",(request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            Squad squad = Squad.find(Integer.parseInt(request.params("id")));
-            model.put("squad", squad);
+            int squadHeroId=Integer.parseInt(request.params("id"));
+            Squad squadId = Squad.findSquadById(squadHeroId);
+            model.put("squad", squadId);
 //            model.put("template", "templates/squad.vtl");
             return new ModelAndView(model, "squads.hbs");
         }, new HandlebarsTemplateEngine());
 
 
-
-
         // route to handle a form for adding new heroes to squads specific squad using the squad id
-        get("squads/:id/heroes/new", (request, response) -> {
-            Map<String, Object> model = new HashMap<String, Object>();
-            Squad squad = Squad.find(Integer.parseInt(request.params("id")));
-            model.put("squad", squad);
-//            model.put("template", "templates/squadHeroesForm.vtl");
-            return new ModelAndView(model, "squad-hero-form.hbs");
-        }, new HandlebarsTemplateEngine());
+//        get("squads/:id/heroes/new", (request, response) -> {
+//            Map<String, Object> model = new HashMap<String, Object>();
+//            Squad squadHeroId = Squad.findSquadById(Integer.parseInt(request.params("id")));
+//            model.put("squadHeroId", squadHeroId);
+////            model.put("template", "templates/squadHeroesForm.vtl");
+//            return new ModelAndView(model, "hero-form.hbs");
+//        }, new HandlebarsTemplateEngine());
 
 
     }
