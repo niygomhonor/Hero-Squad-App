c598a90cd6b80c059f851dbe45e6264a3575c8b6
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-09-18 17:09:10 +0200

    EDIT

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -17,29 +17,33 @@ public class HeroTest {
 
     @Test
     public void Hero_instantiating_Done_Correctly() {
-Hero amHero=new Hero("Honorine",20,"Ever present","Calm");
+        Hero amHero = new Hero("Honorine", 20, "Ever present", "Calm");
     }
+
     @Test
     public void getName_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
+        Hero myHero = new Hero("Vick", 65, "madem", "madem");
         String expected = "Vick";
         assertEquals(expected, myHero.getName());
     }
+
     @Test
     public void getAge_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
+        Hero myHero = new Hero("Vick", 65, "madem", "madem");
         int expected = 65;
         assertEquals(expected, myHero.getAge());
     }
+
     @Test
     public void getWeakness_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
+        Hero myHero = new Hero("Vick", 65, "madem", "madem");
         String expected = "madem";
         assertEquals(expected, myHero.getWeakness());
     }
+
     @Test
     public void getPower_forHeroesName_String() {
-        Hero myHero = new Hero("Vick",65,"madem","madem");
+        Hero myHero = new Hero("Vick", 65, "madem", "madem");
         String expected = "madem";
         assertEquals(expected, myHero.getPower());
     }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -9,21 +9,29 @@ public class SquadTest {
 
     @Test
     public void squadInstancesCreated() {
-        Squad squadTest= new Squad(" Akia","Tank",4);
+        Squad squadTest= new Squad(" Akim","Tank",4);
         assertEquals(true,squadTest instanceof Squad);
     }
 
     @Test
-    public void getName_squadInstanceWith_Nameand_Cause() {
-        Squad squadTest= new Squad(" superMan","Tank",4);
-assertEquals("superMan",squadTest.getsName());
+    public void getName_squadInstanceWith_Name_and_Cause() {
+        Squad squadTest= new Squad(" Akim","Tank",4);
+        String expected="Akim";
+assertEquals("Akim",squadTest.getsName());
     }
 
     @Test
     public void getSize_Of_Squad() {
-        Squad squadTest= new Squad(" Akia","Tank",4);
+        Squad squadTest= new Squad(" Akim","Tank",4);
+        int expected=4;
         assertEquals(4, squadTest.getsSize());
     }
 
+    @Test
+    public void getCauseOf_Squad() {
+        Squad squadTest= new Squad(" Akim","Tank",4);
+        String expected="Tank";
+        assertEquals("Tank", squadTest.getsCause());
+    }
 
 }
